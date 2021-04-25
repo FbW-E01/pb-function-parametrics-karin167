71fddb00b7f9b94f3e44ea8c9eca5443eeeddc89
karin yahud <karin167bz@gmail.com>
2021-04-26 00:02:09 +0200

    fix the task

--- sum.js
+++ sum.js
@@ -1,7 +1,32 @@
 // Create a function sum which accepts any amount of numbers and return the sum of their addition
+//Have 3 ways :
 
-const sum = () => {};
+//A
+// const sum3 = (...args) =>  args.reduce((acc,v)=>acc+v);
+ 
 
+//B
+//const sum2 = (...args) =>{
+// let res=0;
+//  args.forEach(value=>res+=value);
+// return res;
+// }
+
+
+//C
+const sum = (...args) =>{
+  let res=0;
+  for(let i=0;i<args.length;i++){
+    res+=args[i];
+  }
+  return res
+}
+
+console.log(sum(1)); // -> 1
+console.log(sum(5, 15)); // -> 20
+console.log(sum(25, 25, 50)); // -> 100
 console.log(sum(1)); // -> 1
 console.log(sum(1, 15)); // -> 16
 console.log(sum(25, 25, 20)); // -> 70
+
+
